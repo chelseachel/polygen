b8a155b742a85f416079d9ddcc94ec0348ddb919
chelseachel <cheese.hyq@gmail.com>
2025-02-12 12:10:23 +0800

    build: build pages

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/polygen/',
   build: {
     outDir: '../dist/example',
     emptyOutDir: true
